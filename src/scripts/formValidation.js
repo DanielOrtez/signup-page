31d71820dc0016ad9c28bdc4257abfc22633e4a7
Daniel Ortez <ihavebeeneatingpizza.es@gmail.com>
2020-11-21 17:39:28 +0100

    all code refactorized

--- src/scripts/formValidation.js
+++ src/scripts/formValidation.js
@@ -1,12 +1,13 @@
-const form = document.querySelector('form');
+const form = document.getElementById('form');
 form.addEventListener('submit', (event) => {
-    const input = document.querySelectorAll('input[type="text"], input[type="password"], input[type="email"]');
-    for (let index = 0; index < input.length; index++) {
-        if(input[index].value.trim() == '') {
-            event.preventDefault();
-            input[index].classList.add('error_input');
+    event.preventDefault();
+    const input = document.querySelectorAll('input');
+    const n = input.length;
+    for (let index = 0; index < n; index++) {
+        if(input[index].value.trim() === '') {
+            input[index].classList.add('error__input');
         } else {
-            input[index].classList.remove('error_input');
+            input[index].classList.remove('error__input');
         }
     }
 });
--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-import '../styles/index.scss';
-import '../img/bg-intro-desktop.png';
-import '../img/bg-intro-mobile.png';
-import '../img/icon-error.svg';
-import "./formValidation.js";
